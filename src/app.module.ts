540cc4637c735f0643ae1abf373ffc4b1055d182
Sugeng Winanjuar <winanjuar@gmail.com>
2023-01-13 17:10:34 +0700

    remove unused dependency

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,3 @@
-import { RedisModule } from '@liaoliaots/nestjs-redis';
 import { BullModule } from '@nestjs/bull';
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
@@ -9,16 +8,6 @@ import { TaskProcessor } from './tasks/task-processor';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    RedisModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => ({
-        config: {
-          host: configService.get<string>('REDIS_HOST'),
-          port: configService.get<number>('REDIS_PORT'),
-        },
-      }),
-    }),
     BullModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
